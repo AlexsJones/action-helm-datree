4e95d903a3838e9253d5c5e56e59d61fcdfb117d
Alex Jones <alexsimonjones@gmail.com>
2022-01-04 11:28:17 +0000

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-result="Hello ${INPUT_DATREE_TOKEN}"
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm plugin install https://github.com/datreeio/helm-datree
+helm datree test ${INPUT_CHART_DIRECTORY} -- --values ${INPUT_VALUES_FILE} > output
+result=$(cat output)
 echo "::set-output name=result::$result"
