6f5aec4e10eb6dbc741a463221fcd2e9d4fc9395
Alex Jones <alexsimonjones@gmail.com>
2022-01-04 11:33:49 +0000

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-apt install libssl-dev -y
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-helm plugin install https://github.com/datreeio/helm-datree
-helm datree test ${INPUT_CHART_DIRECTORY} -- --values ${INPUT_VALUES_FILE} > output
+wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+tar xvf helm-v3.4.1-linux-amd64.tar.gz
+chmod +x linux-amd64/helm
+./linux-amd64/helm plugin install https://github.com/datreeio/helm-datree
+./linux-amd64/helm datree test ${INPUT_CHART_DIRECTORY} -- --values ${INPUT_VALUES_FILE} > output
 result=$(cat output)
 echo "::set-output name=result::$result"
