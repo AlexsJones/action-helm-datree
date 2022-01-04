d23a86ce5302df2bd43a06fd1719edcb7de08a8d
Alex Jones <alexsimonjones@gmail.com>
2022-01-04 11:31:22 +0000

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-apt-get install libssl-dev -y
+apt install libssl-dev -y
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 helm plugin install https://github.com/datreeio/helm-datree
 helm datree test ${INPUT_CHART_DIRECTORY} -- --values ${INPUT_VALUES_FILE} > output
